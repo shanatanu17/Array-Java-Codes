3b21d1d99d1a164cfba0015e9d549ea35970358d
shantanu17 <shantanushinde3292@gmail.com>
2025-08-21 13:10:12 +0530

    First commit

---
+++ Find_missing_value_in_array.java
@@ -0,0 +1,34 @@
+public class Find_missing_value_in_array
+{
+  public static void main(String ss[])
+  {
+     int arr[] = new int[]{1,4,6,7,11,15};
+	 
+	 
+	 // this is done , bcz we need to check which is the last element of array and by considering that element 
+	 // we need to print the missing element 
+	 int last = arr[arr.length-1];
+		
+	 int i=0;
+	 int j=1;
+	 
+	 
+	 while(i <arr.length)
+	 {
+		 if(arr[i] == j)
+		 {
+			 i++;
+			 j++;
+		 }
+		 else
+		 {
+			 System.out.println(j);
+			 j++;
+		 }
+	 }
+	 
+	 
+	 
+	 
+  }
+}
