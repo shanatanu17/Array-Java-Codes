fcd83bbe9e7c73eb10950065729f3bc8bfe2dbaf
shantanu17 <shantanushinde3292@gmail.com>
2025-08-22 15:25:51 +0530

    First commit

---
+++ Find_prime_numbers_in_array_Correct.java
@@ -0,0 +1,32 @@
+public class Find_prime_numbers_in_array_Correct
+{
+    public static void main(String ss[])
+	{
+	    int arr[] = new int[]{8,13,4,6,23,19,17,37,39,123};
+		int n = arr.length;
+		
+		
+		for(int i=0;i<n;i++)
+		{
+	        int currele = arr[i];
+            int count=0;
+            int k=1;
+
+            while(k <= currele)
+             {
+			    if(currele % k == 0)
+				 {
+				    count++;
+				 }
+			  k++;
+			 }	
+
+             if(count <= 2)
+              {
+			     System.out.println(arr[i]);
+			  }			 
+	 	}
+	 
+	
+	}
+}
