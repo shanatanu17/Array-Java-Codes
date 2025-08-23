7b7135ad7c3ab027b0be538edab51a3e5b503679
shantanu17 <shantanushinde3292@gmail.com>
2025-08-23 14:31:18 +0530

    First commit

---
+++ Insert_at_Specific_index_Correct.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Insert_at_Specific_index_Correct
+{
+    public static void main(String ss[])
+	{
+	
+	    Scanner sc = new Scanner(System.in);
+		
+		
+	    int arr[] = new int[6];
+		
+		
+		System.out.println("Enter the index value u want to insert the element");
+		int index = sc.nextInt();
+		
+		System.out.println("Enter the Element value to ins");
+		int element= sc.nextInt();
+		
+		System.out.println("Enter the array elements which is of size n-1");
+		
+		for(int i=0;i<arr.length-1;i++)
+		{
+		   arr[i] = sc.nextInt();
+		}
+		
+		
+		// traverse from last index of array
+		for(int i=arr.length-2;i>=index;i--)
+		{
+		      arr[i+1]= arr[i];
+		}
+		
+		arr[index] = element;
+		
+		
+		// display the array elements after inserting at specific index
+		
+		for(int i=0;i<arr.length;i++)
+		{
+		   System.out.print(arr[i]+ " ");
+		}
+		
+		
+		
+		
+	}
+}
