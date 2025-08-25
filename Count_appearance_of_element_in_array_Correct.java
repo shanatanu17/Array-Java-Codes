83c20839e819c678b68fb445b480e7b9f2dffe61
shantanu17 <shantanushinde3292@gmail.com>
2025-08-25 17:45:07 +0530

    First commit

---
+++ Count_appearance_of_element_in_array_Correct.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Count_appearance_of_element_in_array_Correct
+{
+    public static void main(String ss[])
+	{
+		Scanner sc = new Scanner(System.in);
+		
+	    int arr[] = new int[]{1, 2, 3, 2, 4, 2, 5 , 22,33,123,4,5,77,4,13,4,88,-46,4};
+		
+		System.out.println("Enter the search key ");
+		int searchelement = sc.nextInt();
+		int count = 0;
+		
+		
+		for(int i = 0;i<arr.length;i++)
+		{
+			if(arr[i] == searchelement)
+			{
+				count++;
+			}
+		}
+		
+		if(count == 0)
+		{
+		    System.out.println("Search element is not present in array");
+		}
+		else
+		{
+			System.out.println("Search element is present for-->  " + count + "  no of times " );
+		}
+	
+	}
+}
